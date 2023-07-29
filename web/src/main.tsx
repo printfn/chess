28637377983c57f30f2751f33dc87a8d92382ea5
printfn <printfn@users.noreply.github.com>
2023-07-29 00:10:48 +0000

    Remove old code

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -8,22 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		<App />
 	</React.StrictMode>,
 );
-
-// import './style.scss';
-// import { Chessground } from 'chessground';
-// import { Config } from 'chessground/config';
-// import { greet } from '../../wasm/pkg';
-
-// const config: Config = {
-// 	movable: {
-// 		free: false,
-// 		dests: new Map([['e7', ['e6', 'e5']]]),
-// 	},
-// 	animation: {
-// 		enabled: true,
-// 	},
-// };
-// const chessElement = document.getElementById('chessboard')!;
-// const board = Chessground(chessElement, config);
-// board.move('e2', 'e4');
-// greet();
