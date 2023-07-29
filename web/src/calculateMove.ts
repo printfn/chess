0d02fc8b3edbc95c29ed47584c40101196998c57
printfn <printfn@users.noreply.github.com>
2023-07-29 22:46:04 +0000

    Use async/await in web worker

--- web/src/calculateMove.ts
+++ web/src/calculateMove.ts
@@ -1,10 +1,8 @@
 import { default as initWasm, calculate_move } from '../../wasm/pkg';
 
-self.addEventListener('message', ({ data }) => {
-	initWasm().then(() => {
-		console.log('calculateMove.ts: data = ', data);
-		const fen: string = data;
-		const result = calculate_move(fen);
-		postMessage(result);
-	});
+self.addEventListener('message', async ({ data }) => {
+	await initWasm();
+	const fen: string = data;
+	const result = calculate_move(fen);
+	postMessage(result);
 });
