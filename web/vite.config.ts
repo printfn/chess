3e349a9595deca6a2da4d849df49920d6b04eff5
printfn <printfn@users.noreply.github.com>
2023-07-30 05:04:10 +0000

    Fix production build sourcemap config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	build: {
 		target: 'esnext',
-		rollupOptions: {
-			output: {
-				sourcemap: true,
-			},
-		},
+		sourcemap: true,
 	},
 	server: {
 		fs: {
