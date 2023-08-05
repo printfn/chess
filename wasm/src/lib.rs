73cc888c9597cc3b8e21d62e303bcba66d360956
printfn <printfn@users.noreply.github.com>
2023-08-05 02:04:11 +0000

    Fix typescript definition

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,7 +3,7 @@ use std::ops;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(typescript_custom_section)]
-const ITEXT_STYLE: &'static str = r#"
+const _: &'static str = r#"
 type File = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
 type Rank = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
 type Square = `${File}${Rank}`;
