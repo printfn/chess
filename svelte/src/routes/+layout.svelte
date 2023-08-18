5eddef8207b491bf156d56288942a9a299734dfe
printfn <printfn@users.noreply.github.com>
2023-08-18 12:24:44 +0000

    Apply theme to board

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -3,14 +3,19 @@
 	import '../app.css';
 
 	theme.subscribe(theme => {
+		const root = document.querySelector(':root') as HTMLElement;
+		if (!root) {
+			return;
+		}
 		const name = theme === 'pink' ? 'carissma' : 'driftwood';
 		const numbers = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
 		for (const n of numbers) {
-			(document.querySelector(':root') as HTMLElement)?.style.setProperty(
+			root.style.setProperty(
 				`--color-primary-${n}`,
 				`var(--${name}-${n})`,
 			);
 		}
+		root.setAttribute('data-theme', theme);
 	});
 </script>
 
