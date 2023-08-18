a43e67b73e6c7e9fde638b5c5d6e211d1cc5a3e4
printfn <printfn@users.noreply.github.com>
2023-08-18 11:26:59 +0000

    Sync theme with local storage

--- svelte/src/lib/settings.ts
+++ svelte/src/lib/settings.ts
@@ -1,8 +1,19 @@
 import { writable } from 'svelte/store';
 
-export const Themes = ['pink', 'brown'] as const;
-export type Theme = (typeof Themes)[number];
+export const themes = ['pink', 'brown'];
+export type Theme = 'pink' | 'brown';
 
-export const theme = writable<Theme>('pink');
+function initialTheme(): Theme {
+	const theme = localStorage.getItem('theme');
+	if (theme && themes.includes(theme)) {
+		return theme as Theme;
+	}
+	return 'pink';
+}
+
+export const theme = writable<Theme>(initialTheme());
+theme.subscribe(theme => {
+	localStorage.setItem('theme', theme);
+});
 export const depth = writable(3);
 export const enableQuiescence = writable(true);
