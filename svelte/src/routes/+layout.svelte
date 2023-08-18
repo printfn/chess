95176e412cc135512198b9a368d86eed6dc72628
printfn <printfn@users.noreply.github.com>
2023-08-18 12:27:08 +0000

    Simplify theme switching

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -7,14 +7,6 @@
 		if (!root) {
 			return;
 		}
-		const name = theme === 'pink' ? 'carissma' : 'driftwood';
-		const numbers = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950];
-		for (const n of numbers) {
-			root.style.setProperty(
-				`--color-primary-${n}`,
-				`var(--${name}-${n})`,
-			);
-		}
 		root.setAttribute('data-theme', theme);
 	});
 </script>
