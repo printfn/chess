00f0c645bebeb4da721299c5a7d054dd76ed0c56
printfn <printfn@users.noreply.github.com>
2025-02-15 11:01:50 +0000

    tailwind v4

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	build: {
 		target: 'esnext',
 		sourcemap: true,
 	},
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	server: {
 		fs: {
 			allow: [searchForWorkspaceRoot(process.cwd()), '../wasm/pkg/'],
